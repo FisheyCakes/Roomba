db084d79a06acdae4caa921ea4a8626a797a26c3
tallyhawley <league@WTS5.attlocal.net>
2017-04-17 18:26:51 -0700

    Roomba

---
+++ src/level2/Challenge.java
@@ -0,0 +1,27 @@
+package level2;
+
+import level1.Sonar;
+import processing.core.PApplet;
+
+public class Challenge extends Head {
+	Roomba roomba;
+
+	public static void main(String[] args) {
+		PApplet.main("level2.Processing");
+	}
+
+	public Challenge(Roomba roomba) {
+		super(roomba);
+		Sonar sonar = new Sonar(roomba);
+	}
+
+	public void initialize() {
+driveDirect(500, 500);
+sleep(3000);
+driveDirect(0, 500);
+	}
+
+	public void loop() {
+	
+	}
+}
